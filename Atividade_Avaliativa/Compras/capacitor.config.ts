0805cce480b541e2676b3ba9c982a7dca3622593
Fellipe Souza Maia <fellipesouzamaia1@gmail.com>
2025-05-30 14:13:52 -0300

    30/05

--- Atividade_Avaliativa/Compras/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Compras',
-  webDir: 'www'
-};
-
-export default config;
